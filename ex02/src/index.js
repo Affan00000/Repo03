28699c01fde864e534e1fde32d3d50f080dcd51a
Affan00000 <90653323+Affan00000@users.noreply.github.com>
2021-09-16 03:10:37 +0200

    Add files via upload

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+    // Only change code below this line
+
+    // console.log('Hello world!'); <--single line comment
+
+     /* This is
+          a
+      multi-line
+    comment......*/
+    
+    // Only change code above this line
+   
+    }
+
+main();
+
+module.exports = main;
---
+++ ex07/src/index.js
@@ -0,0 +1,27 @@
+function main(){
+    // Only change code below this line
+    // Variable declarations
+    
+    var studylyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+    
+    // Variable assigments
+
+    studylyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+
+    return {
+    studylyCapVar,
+    properCamelCase,
+    titleCaseOver
+    }
+
+
+    // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
